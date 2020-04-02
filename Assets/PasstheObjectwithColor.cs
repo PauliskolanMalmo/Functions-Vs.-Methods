b86bcd68d242c433e85562586accef3e62057815
Paul B <paul.belfrage@gmail.com>
2020-04-02 14:11:45 +0200

    Challenge Review Pass the Object with Color.
    Challenge Review Pass the Object with Color.

---
+++ Assets/PasstheObjectwithColor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PasstheObjectwithColor : MonoBehaviour
+{
+    public GameObject cube;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeColor(cube, Color.blue);
+        }
+    }
+
+    private void ChangeColor(GameObject obj, Color colorToAssign)
+    {
+        obj.GetComponent<MeshRenderer>().material.color = colorToAssign;
+    }
+}
