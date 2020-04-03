ed0a6f65853d4a25e736f2b6cf3b2307232f3e4c
Paul B <paul.belfrage@gmail.com>
2020-04-03 14:33:38 +0200

    init
    init

---
+++ Assets/Are You Alive.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreYouAlive : MonoBehaviour
+{
+    public bool isDead;
+    public int health = 100;
+
+    public void Start()
+
+    {
+
+    }
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isDead == false)
+        {
+            Damage(Random.Range(5, 20));
+        }
+    }
+    public void Damage(int damageAmount)
+    {
+        health -= damageAmount;
+        if (health < 1)
+        {
+            isDead = true;
+            print("The player hast died");
+        }
+    }
+}
+
