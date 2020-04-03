8a1b492feabd29474b3e58976ae4898acb7a5cbc
Paul B <paul.belfrage@gmail.com>
2020-04-03 14:40:27 +0200

    Challenge Review Are You Alive
    Challenge Review Are You Alive

--- Assets/AreYouAlive.cs
+++ Assets/AreYouAlive.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AreYouAlive : MonoBehaviour
 {
-    public bool isDead;
+    //public bool isDead;
     public int health = 100;
 
     public void Start()
@@ -16,7 +16,7 @@ public class AreYouAlive : MonoBehaviour
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isDead == false)
+        if (Input.GetKeyDown(KeyCode.Space) && IsDead() == false /*&& isDead == false*/)
         {
             Damage(Random.Range(5, 20));
         }
@@ -24,11 +24,25 @@ public class AreYouAlive : MonoBehaviour
     public void Damage(int damageAmount)
     {
         health -= damageAmount;
-        if (health < 1)
+
+        if (IsDead() == true)
         {
-            isDead = true;
+            health = 0;
             print("The player hast died");
         }
+        /*
+        if (health < 1)
+        {
+            //isDead = true;
+            health = 0;
+            print("The player hast died");
+        }
+        */
+    }
+
+    public bool IsDead()
+    {
+        return health < 1;
     }
 }
 
