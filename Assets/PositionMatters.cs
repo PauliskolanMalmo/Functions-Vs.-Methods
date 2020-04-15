95d9a1474174f785c7ba94b192b778806ecda9fa
Paul B <paul.belfrage@gmail.com>
2020-04-15 12:06:26 +0200

    Challenge Position Matters
    Challenge Position Matters

---
+++ Assets/PositionMatters.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionMatters : MonoBehaviour
+{
+    public Vector3[] positions;
+    private int randomIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomIndex = GetRandom();
+        print("randomIndex" + randomIndex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    int GetRandom()
+    {
+        return Random.Range(0, positions.Length);
+    }
+
+    Vector3 GetPosition()
+    {
+        return positions[randomIndex];
+    }
+}
